4dfee7316beb50316acc07a7ea23b48d84103b85
vminozzi <vin.minozzi@gmail.com>
2015-09-03 09:20:18 -0300

    versão pronta para testes

--- AVCB/Source/Controllers/Home.m
+++ AVCB/Source/Controllers/Home.m
@@ -313,7 +313,13 @@
 #pragma mark - IBActions
 - (IBAction)btnScannerQR:(id)sender
 {
-    //[self chamadaServico:@{@"chave":  @"29B4CAC75FEDB50C683E6803B4580716"}];
+    /*
+                Chamada para testar no simulador 
+     
+     
+     [self chamadaServico:@{@"chave":  @"29B4CAC75FEDB50C683E6803B4580716"}];
+     
+    */
     [self presentViewController:_reader animated:YES completion:nil];
 }
 
